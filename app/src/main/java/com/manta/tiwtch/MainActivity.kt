42f2ce6868a5bc3bb724efb30905a790671246c0
Tim <bso11246@gmail.com>
2022-08-17 22:14:53 +0900

    init

---
+++ app/src/main/java/com/manta/tiwtch/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.manta.tiwtch
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.manta.tiwtch.ui.theme.TiwtchTheme
+
+//twitch token: boxgcgv7wxdd55tytip9pbt7r2u4fk
+//twitch client id: elpvgbj4uevz61bh2v96scwcen7q5b
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TiwtchTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TiwtchTheme {
+        Greeting("Android")
+    }
+}
